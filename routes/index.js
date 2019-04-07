a5da148f16531c16c9741dd3b919a0c4e5409fd2
Avik Kundu <kundu.abheek1999@gmail.com>
2019-04-07 19:03:37 +0530

    Update index.js

--- routes/index.js
+++ routes/index.js
@@ -6,8 +6,8 @@ const { ensureAuthenticated } = require('../config/auth');
 const bodyParser = require('body-parser');
 // const bootstrap = require('bootstrap');
 
-const Post = require('./models/Post');
-const User = require('./models/User');
+const Post = require('../models/Post');
+const User = require('../models/User');
 router.use(expressLayouts); 
 router.use(bodyParser.urlencoded({ extended: true }))
 // const posts = [
