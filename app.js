fca4ef7000c0c1accd5a86ca3693fdb96e379197
Avik Kundu <kundu.abheek1999@gmail.com>
2019-04-07 16:12:17 +0530

    Update app.js

--- app.js
+++ app.js
@@ -99,7 +99,6 @@ app.use(function(req, res, next) {
 // Routes
 app.use('/', require('./routes/index.js'));
 app.use('/users', require('./routes/users.js'));
-app.use('/users', require('./routes/users.js'));
 
 const PORT = process.env.PORT || 5000;
 
