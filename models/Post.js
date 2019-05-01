ca3e70bc0af9652ed55c09fd1b3f7c659eb8b788
Avik Kundu <1828008@kiit.ac.in>
2019-05-01 20:56:40 +0530

    added Author field

--- models/Post.js
+++ models/Post.js
@@ -6,6 +6,7 @@ const PostSchema = new mongoose.Schema({
     description: String,
     content: String,
     username: String,
+    author:  String,
     createdAt: {
         type: Date,
         default: new Date()
